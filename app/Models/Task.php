49e4b4311aafbd7ae1fcd1e2173463418a262d66
Tiago Guardado <82142114+tiaguardado@users.noreply.github.com>
2025-06-17 16:38:55 +0100

    Relações

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -17,4 +17,8 @@ class Task extends Model
     protected $casts = [
         'completed' => 'boolean',
     ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,7 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function tasks(){
+        return $this->hasMany(Task::class);
+    }
 }
